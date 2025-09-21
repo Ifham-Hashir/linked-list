af2857873637a69aff94ec868f7afc21f537a4ce
Ifham-Hashir <ifham09052003@gmail.com>
2025-09-21 22:06:48 +0530

    Add append function

--- script.js
+++ script.js
@@ -1,130 +1,35 @@
-function node(value) {
-  return {
-    value,
-    nextNode: null,
-  }
+function Node(value = null, nextNode = null) {
+  return {value, nextNode}
 }
 
-function linkedList() {
+function LinkedList(){
   return {
     head: null,
     tail: null,
-    size: 0,
+    length: 0,
     append(value) {
-      const newNode = node(value);
-
-      if (this.head === null){
+      let newNode = Node(value);
+      if (this.length === 0){
         this.head = newNode;
-        this.tail = newNode;
-        this.size++;
-        return newNode;
       }
-
-      this.tail.nextNode = newNode;
+      else{
+        this.tail.nextNode = newNode;
+      }
       this.tail = newNode;
-      this.size++;
-      return newNode;
+      this.length++;
     },
-    prepend(value) {
-      const newNode = node(value);
 
-        if (this.head === null){
-        this.head = newNode;
-        this.tail = newNode;
-        this.size++;
-        return newNode;
-      }
+    size() {
+      return this.length;
+    },
 
-      newNode.nextNode = this.head
-      this.head = newNode;
-      this.size++;
-      return newNode;
-    },
-    at(index) {
-      let currentNode = this.head;
-      let counter = 0;
-      while(currentNode !== null){
-        if(counter === index){
-          return currentNode;
-        }
-        currentNode = currentNode.nextNode;
-        counter++;
-      }
-      return null;
-    },
-    pop() {
-      if(this.size === 0){
-        return null;
-      }
-      const node = this.tail;
-
-      if(this.head === this.tail){
-        this.head = null;
-        this.tail = null;
-        this.size--;
-        return node;
-      }
-      let currentNode = this.head;
-      while(currentNode){
-        if(currentNode.nextNode.nextNode === null){
-          currentNode.nextNode = null;
-          this.tail = currentNode;
-          this.size--;
-          return node;
-        }
-        currentNode = currentNode.nextNode;
-      }
-    },
-    contains(value){
-      if(this.size === 0){
-        return false;
-      }
-
-      let currentNode = this.head;
-      while(currentNode){
-        if(currentNode.value === value){
-          return true;
-        }
-        currentNode = currentNode.nextNode;
-      }
-      return false;
-    },
-    find(value){
-      if(this.size === 0){
-        return null;
-      }
-      let currentNode = this.head;
-      let index = 0;
-      while(currentNode){
-        if(currentNode.value === value){
-          return index;
-        }
-        currentNode = currentNode.nextNode;
-        index++;
-      }
-      return null;
-    },
-    toString() {
-      let string = ""
-      let currentNode = this.head;
-      while(currentNode){
-        string += `(${currentNode.value}) -> `
-        currentNode = currentNode.nextNode;
-      }
-      string += "null";
-      return string;
-    }
   }
 }
 
-// example uses class syntax - adjust as necessary
-const list = linkedList();
+let list = LinkedList()
+list.append(8)
+list.append(10)
+list.append(15)
+console.log(list)
 
-list.append("dog");
-list.append("cat");
-list.append("parrot");
-list.append("hamster");
-list.append("snake");
-list.append("turtle");
 
-console.log(list.toString())

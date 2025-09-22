330e5906419203b860318a64d6cc5cd1e21f539f
Ifham-Hashir <ifham09052003@gmail.com>
2025-09-22 20:57:15 +0530

    Add at(index) function

--- script.js
+++ script.js
@@ -19,17 +19,42 @@ function LinkedList(){
       this.length++;
     },
 
+    prepend(value) {
+      let newNode = Node(value, this.head);
+      this.head = newNode;
+      if(this.length === 0){
+        this.tail = newNode;
+      }
+      this.length++;
+    },
+
     size() {
       return this.length;
+    },
+
+    at(index) {
+      let currIndex = 0;
+      let currNode = this.head;
+      if (index < this.length && index >= 0){
+        while(currIndex < index){
+          currNode = currNode.nextNode;
+          currIndex++;
+        }
+        return currNode;
+      }
+      return null;
     },
 
   }
 }
 
 let list = LinkedList()
-list.append(8)
-list.append(10)
-list.append(15)
+list.append(4)
+list.append(5)
+list.prepend(3)
 console.log(list)
+list.pop()
+console.log(list)
+
 
 
